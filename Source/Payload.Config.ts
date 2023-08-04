9ab87009c326df54f55c2798e6e758a3bef8444e
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-05 01:00:45 +0300

    squash!

--- Source/Payload.Config.ts
+++ Source/Payload.Config.ts
@@ -1,7 +1,7 @@
 import { resolve as Resolve } from "path";
 import { buildConfig } from "payload/config";
 import Users from "./collections/Users";
-import Environment from "./Lib/Environment";
+import Environment from "./Library/Environment";
 
 export default buildConfig({
 	serverURL: "http://localhost:3000",
